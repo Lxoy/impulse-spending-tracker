57cc71c511a9cb8b9b533d58f0fdff3acdcfa66c
Lxoy <lo14loyt@gmail.com>
2026-03-19 13:47:58 +0100

    Add project files.

---
+++ impulse-spending-tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace impulse_spending_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
